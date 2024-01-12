5de0b9110e211cad002788ca324a143ccebe2a9c
Fazil <muhammed.fazil@skizzle.tech>
2024-01-12 15:36:22 +0530

    code updated

---
+++ ios/Frame Processor/Frame.h
@@ -0,0 +1,32 @@
+//
+//  Frame.h
+//  VisionCamera
+//
+//  Created by Marc Rousavy on 15.03.21.
+//  Copyright © 2021 mrousavy. All rights reserved.
+//
+
+#pragma once
+
+#import <CoreMedia/CMSampleBuffer.h>
+#import <Foundation/Foundation.h>
+#import <UIKit/UIImage.h>
+
+@interface Frame : NSObject
+
+- (instancetype _Nonnull)initWithBuffer:(CMSampleBufferRef _Nonnull)buffer orientation:(UIImageOrientation)orientation;
+
+@property(nonatomic, readonly) CMSampleBufferRef _Nonnull buffer;
+@property(nonatomic, readonly) UIImageOrientation orientation;
+
+// Getters
+- (NSString* _Nonnull)pixelFormat;
+- (BOOL)isMirrored;
+- (BOOL)isValid;
+- (size_t)width;
+- (size_t)height;
+- (double)timestamp;
+- (size_t)bytesPerRow;
+- (size_t)planesCount;
+
+@end
